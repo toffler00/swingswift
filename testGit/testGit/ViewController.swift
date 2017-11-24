b152a86ed5a9f80845e5d8c38772b56a74634a0c
toffler00 <5chunwon@gmail.com>
2017-11-24 11:49:11 +0900

    addSqaure

--- testGit/testGit/ViewController.swift
+++ testGit/testGit/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var square : UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        square.frame = CGRect(x: 100, y: 100, width: 100, height: 100)
+        square.backgroundColor = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
+        square.setTitle("TEST BUTTON", for: .normal)
+        square.setTitleColor(.white, for: .normal)
+        self.view.addSubview(square)
     }
 
     override func didReceiveMemoryWarning() {
